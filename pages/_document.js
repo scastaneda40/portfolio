1249f0f193d8d2a842443d9d84d1e7756293e3aa
Stephen Castaneda <stephencastaneda@MacBookPro.lan>
2024-12-09 19:39:29 -0800

    updated head tag

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,13 @@ import { Head, Html, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+      <meta property="og:title" content="Stephen&apos;s Dev Portfolio" />
+        <meta property="og:description" content="Welcome to Stephe&apos;s portfolio. Feel free to explore my most recent projects and download my resume to learn more about my work!" />
+        <meta property="og:image" content="/images/stephen.jpeg" />
+        <meta property="og:url" content="https://www.stevethebuilder.dev/" />
+        <meta property="og:type" content="website" />
+      </Head>
       <body>
         <Main />
         <NextScript />
